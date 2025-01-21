115e17e20c73fb7e9b68042e9eec9a9bc6e69b1a
Pierre-Yves LANDURÉ <pierre-yves@landure.fr>
2025-01-21 08:59:13 +0100

    ci: :bug: use Docker to run vale CI

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   "packages.json": "npm audit",
-  "*.md": ["npx markdownlint-cli", "npx cspell lint", "vale"],
+  "*.md": [
+    "npx markdownlint-cli",
+    "npx cspell lint",
+    "docker run --rm --workdir '/docs' --volume '.:/docs:ro' jdkato/vale"
+  ],
 };
